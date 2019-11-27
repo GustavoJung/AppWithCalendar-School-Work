04fd00150421199fb11a3621c277c4b022b618db
08205268940 <mr.caraiba@gmail.com>
2019-11-27 16:53:27 -0300

    Calendario na tela
    Login com o google começado

--- app/src/main/java/com/example/trabalhodsd/MainActivity.java
+++ app/src/main/java/com/example/trabalhodsd/MainActivity.java
@@ -1,36 +1,128 @@
 package com.example.trabalhodsd;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final int RC_SIGN_IN = 9001;
     private Button btLogin;
-    private EditText text;
-    private String nome_user;
+    private FirebaseAuth mAuth;
+    private GoogleSignInClient mGoogleSignInClient;
+
+    public MainActivity() {
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        text = findViewById(R.id.nome_user);
 
-
+        mAuth = FirebaseAuth.getInstance();
         btLogin = findViewById(R.id.bt_login);
         btLogin.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                nome_user = text.getText().toString();
-                Intent newActivity = new Intent(MainActivity.this, Telaprincipal.class);
-                newActivity.putExtra("nome", nome_user);
-                startActivity(newActivity);
+            public void onClick(View view) {
+                Intent t = new Intent(MainActivity.this, Telaprincipal.class);
                 finish();
+                startActivity(t);
             }
         });
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
     }
 
-}
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w("login google", "Google sign in failed", e);
+                // ...
+            }
+        }
+    }
+
+        private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+            Log.d("login", "firebaseAuthWithGoogle:" + acct.getId());
+
+            // [END_EXCLUDE]
+
+            AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+            mAuth.signInWithCredential(credential)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d("Login", "signInWithCredential:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w("Login", "signInWithCredential:failure", task.getException());
+                                Toast.makeText(MainActivity.this, "Falha no login", Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    });
+        }
+
+        private void signIn() {
+            Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+            startActivityForResult(signInIntent, RC_SIGN_IN);
+        }
+        // [END signin]
+
+        private void signOut() {
+            // Firebase sign out
+            mAuth.signOut();
+
+            // Google sign out
+            mGoogleSignInClient.signOut().addOnCompleteListener(this,
+                    new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+
+                            System.out.println("logout");
+                        }
+                    });
+        }
+
+    }
--- app/src/main/java/com/example/trabalhodsd/Telaprincipal.java
+++ app/src/main/java/com/example/trabalhodsd/Telaprincipal.java
@@ -1,18 +1,30 @@
 package com.example.trabalhodsd;
 
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.CalendarView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 
 
 public class Telaprincipal extends AppCompatActivity {
+
+    CalendarView mCalendar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_telaprincipal2);
-
+        mCalendar = findViewById(R.id.calendarView);
+        mCalendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int ano, int mes, int dia) {
+                Toast.makeText(Telaprincipal.this, "Data selecionada: " + dia+"/"+ (mes+1) +"/"+ ano, Toast.LENGTH_SHORT).show();
+            }
+        });
         String nome = getIntent().getStringExtra("nome");
         Toast.makeText(this, "Bem vindo " + nome + "!", Toast.LENGTH_SHORT).show();
     }
